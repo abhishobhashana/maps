d82c08a114c956b0cd1709ed8bb4eac3d8864a9b
abhi <abhiashobhashana@gmail.com>
2024-01-10 02:06:59 +0530

    404 page

--- src/pages/error-page.jsx
+++ src/pages/error-page.jsx
@@ -1,15 +1,12 @@
-import { useRouteError } from "react-router-dom";
+import React from "react";
 
 export default function ErrorPage() {
-  const error = useRouteError();
-
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div className="min-h-screen flex flex-col items-center justify-center gap-5">
+      <h1 className="text-2xl text-secondary dark:text-dark-grey">Oops!</h1>
+      <span className="text-secondary dark:text-dark-grey">
+        Sorry, an unexpected error has occurred.
+      </span>
     </div>
   );
 }
